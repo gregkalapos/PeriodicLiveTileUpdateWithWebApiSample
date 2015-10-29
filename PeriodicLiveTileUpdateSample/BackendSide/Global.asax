ffa41cfa7a3ac77154cfebe85c69688a556c1a72
gergo <gergo@kalapos.net>
2015-10-29 06:35:01 -0700

    Upload sample

---
+++ PeriodicLiveTileUpdateSample/BackendSide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackendSide.WebApiApplication" Language="C#" %>
---
+++ PeriodicLiveTileUpdateSample/BackendSide/Models/TileUpdaterController.cs
@@ -0,0 +1,89 @@
+﻿using NotificationsExtensions.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace BackendSide.Models
+{
+    public class TileUpdaterController : ApiController
+    {
+
+        public HttpResponseMessage GetNewTile()
+        {
+            var response = new HttpResponseMessage()
+            {
+                Content = new StringContent(Generator.Generate().GetContent(), Encoding.UTF8, "application/xml")
+            };
+
+            return response;
+        }
+
+    }
+
+    class Generator
+    {
+        public static TileContent Generate()
+        {
+            Random rnd = new Random(DateTime.Now.Second);
+
+            TileContent content = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+                    Branding = TileBranding.NameAndLogo,
+
+                    TileSmall = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                            {
+                                new TileText() { Text = "PeriodicSmall" + rnd.Next(30) }            
+                            }
+                        }
+                    },
+
+                    TileMedium = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                {
+                    new TileText() { Text = "PeriodicMedium" + rnd.Next(30) }
+                }
+                        }
+                    },
+
+                    TileWide = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                {
+                    new TileText() { Text = "PeriodicWide" + rnd.Next(30) }
+                }
+                        }
+                    },
+
+                    TileLarge = new TileBinding()
+                    {
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                {
+                    new TileText() { Text = "PeriodicLarge" + rnd.Next(30) }
+                }
+                        }
+                    }
+                }
+            };
+
+            return content;
+        }
+    }
+}
+
